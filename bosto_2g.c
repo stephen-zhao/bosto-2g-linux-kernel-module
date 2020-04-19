d61afc059b528bd887b51bc01eda2e5e2f36c057
Mark Ferrell <major@homeonderanged.org>
2020-04-19 11:28:57 -0700

    Attempt to compare Bosto2G to Hanwang driver
    This commit establishes a direct comparison of the Bosto2G tablet driver
    with the Hanwang tablet driver found in the Linux kernel.  The general
    conclusion is that the vast majority of the Bosto2G driver is a simple
    sed string replace of hanwang with bosto2g with the exception of a chunk
    of code in the input stream parser being refactored in the bosto2g
    driver.  It is not yet clear if this refactoring was actually adding
    useful code or if the other was attempting to simplify what they felt
    was a confusing state case.

--- bosto_2g.c
+++ bosto_2g.c
@@ -1,56 +1,28 @@
 /*
- *  USB bosto_2g tablet support
+ *  USB Hanwang tablet support
  *
  *  Copyright (c) 2010 Xing Wei <weixing@hanwang.com.cn>
- *
  */
 
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
-#include <linux/jiffies.h>
-#include <linux/stat.h>
+
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/module.h>
-#include <linux/init.h>
 #include <linux/usb/input.h>
-#include <linux/fcntl.h>
-#include <linux/unistd.h>
 
-#define DRIVER_AUTHOR   "Aidan Walton <aidan@wires3.net>"
-#define DRIVER_DESC     "USB Bosto(2nd Gen) tablet driver"
-#define DRIVER_LICENSE  "GPL"
-
-MODULE_AUTHOR(DRIVER_AUTHOR);
-MODULE_DESCRIPTION(DRIVER_DESC);
-MODULE_LICENSE(DRIVER_LICENSE);
+MODULE_AUTHOR("Xing Wei <weixing@hanwang.com.cn>");
+MODULE_DESCRIPTION("USB Hanwang tablet driver");
+MODULE_LICENSE("GPL");
 
 #define USB_VENDOR_ID_HANWANG		0x0b57
-#define USB_PRODUCT_BOSTO22HD		0x9016
-#define USB_PRODUCT_BOSTO14WA		0x9018
-#define USB_PRODUCT_ART_MASTER_III	0x852a
-#define USB_PRODUCT_ART_MASTER_HD	0x8401
+#define HANWANG_TABLET_INT_CLASS	0x0003
+#define HANWANG_TABLET_INT_SUB_CLASS	0x0001
+#define HANWANG_TABLET_INT_PROTOCOL	0x0002
 
-#define BOSTO_TABLET_INT_CLASS	0x0003
-#define BOSTO_TABLET_INT_SUB_CLASS	0x0001
-#define BOSTO_TABLET_INT_PROTOCOL	0x0002
-
-#define PKGLEN_MAX	10
+#define ART_MASTER_PKGLEN_MAX	10
 
 /* device IDs */
 #define STYLUS_DEVICE_ID	0x02
@@ -59,68 +31,63 @@ MODULE_LICENSE(DRIVER_LICENSE);
 #define ERASER_DEVICE_ID	0x0A
 #define PAD_DEVICE_ID		0x0F
 
-/* match vendor and interface info */
+/* match vendor and interface info  */
+#define HANWANG_TABLET_DEVICE(vend, cl, sc, pr) \
+	.match_flags = USB_DEVICE_ID_MATCH_VENDOR \
+		| USB_DEVICE_ID_MATCH_INT_INFO, \
+	.idVendor = (vend), \
+	.bInterfaceClass = (cl), \
+	.bInterfaceSubClass = (sc), \
+	.bInterfaceProtocol = (pr)
 
-#define BOSTO_TABLET_DEVICE(vend, prod, cl, sc, pr) \
-    .match_flags = USB_DEVICE_ID_MATCH_INT_INFO \
-            | USB_DEVICE_ID_MATCH_DEVICE, \
-        .idVendor = (vend), \
-        .idProduct = (prod), \
-        .bInterfaceClass = (cl), \
-        .bInterfaceSubClass = (sc), \
-        .bInterfaceProtocol = (pr)
-
-
-enum bosto_2g_tablet_type {
-	HANWANG_BOSTO_22HD,
-	HANWANG_BOSTO_14WA,
+enum hanwang_tablet_type {
 	HANWANG_ART_MASTER_III,
 	HANWANG_ART_MASTER_HD,
+	HANWANG_ART_MASTER_II,
+	HANWANG_BOSTO_22HD,
+	HANWANG_BOSTO_14WA,
 };
 
-static const int hw_btnevents[] = {
-/*
-	BTN_STYLUS 		seems to be the same as a center mouse button above the roll wheel.
- 	BTN_STYLUS2		right mouse button (Gedit)
- 	BTN_DIGI		seems to do nothing in relation to the stylus tool
-	BTN_TOUCH		left mouse click, or pen contact with the tablet surface. Remains asserted whenever the pen has contact..
-*/
-
-	//BTN_DIGI, BTN_TOUCH, BTN_STYLUS, BTN_STYLUS2, BTN_TOOL_PEN, BTN_TOOL_BRUSH, BTN_TOOL_RUBBER, BTN_TOOL_PENCIL, BTN_TOOL_AIRBRUSH, BTN_TOOL_FINGER, BTN_TOOL_MOUSE
-	BTN_TOUCH, BTN_TOOL_PEN, BTN_TOOL_RUBBER, BTN_STYLUS
-};
-
-struct bosto_2g {
+struct hanwang {
 	unsigned char *data;
 	dma_addr_t data_dma;
 	struct input_dev *dev;
 	struct usb_device *usbdev;
 	struct urb *irq;
-	const struct bosto_2g_features *features;
+	const struct hanwang_features *features;
 	unsigned int current_tool;
 	unsigned int current_id;
-	unsigned int tool_update;
-	bool stylus_btn_state;
-	bool stylus_prox;
 	char name[64];
 	char phys[32];
 };
 
-struct bosto_2g_features {
+struct hanwang_features {
 	unsigned short pid;
 	char *name;
-	enum bosto_2g_tablet_type type;
+	enum hanwang_tablet_type type;
 	int pkg_len;
 	int max_x;
 	int max_y;
+	int max_tilt_x;
+	int max_tilt_y;
 	int max_pressure;
 };
 
-static const struct bosto_2g_features features_array[] = {
-	{ USB_PRODUCT_BOSTO22HD, "Bosto Kingtee 22HD", HANWANG_BOSTO_22HD,
-		PKGLEN_MAX, 0x27de, 0x1cfe, 0x0800 },
-	{ USB_PRODUCT_BOSTO14WA, "Bosto Kingtee 14WA", HANWANG_BOSTO_14WA,
-		PKGLEN_MAX, 0x27de, 0x1cfe, 0x0800 },
+static const struct hanwang_features features_array[] = {
+	{ 0x8528, "Hanwang Art Master III 0906", HANWANG_ART_MASTER_III,
+	  ART_MASTER_PKGLEN_MAX, 0x5757, 0x3692, 0x3f, 0x7f, 2048 },
+	{ 0x8529, "Hanwang Art Master III 0604", HANWANG_ART_MASTER_III,
+	  ART_MASTER_PKGLEN_MAX, 0x3d84, 0x2672, 0x3f, 0x7f, 2048 },
+	{ 0x852a, "Hanwang Art Master III 1308", HANWANG_ART_MASTER_III,
+	  ART_MASTER_PKGLEN_MAX, 0x7f00, 0x4f60, 0x3f, 0x7f, 2048 },
+	{ 0x8401, "Hanwang Art Master HD 5012", HANWANG_ART_MASTER_HD,
+	  ART_MASTER_PKGLEN_MAX, 0x678e, 0x4150, 0x3f, 0x7f, 1024 },
+	{ 0x8503, "Hanwang Art Master II", HANWANG_ART_MASTER_II,
+	  ART_MASTER_PKGLEN_MAX, 0x27de, 0x1cfe, 0x3f, 0x7f, 1024 },
+	{ 0x9016, "Hanwang Bosto 22HD", BOSTO_22HD,
+	  ART_MASTER_PKGLEN_MAX, 0x27de, 0x1cfe, 0x3f, 0x7f, 2048 },
+	{ 0x9018, "Hanwang Bosto 14WA", BOSTO_14WA,
+	  ART_MASTER_PKGLEN_MAX, 0x27de, 0x1cfe, 0x3f, 0x7f, 2048 },
 };
 
 static const int hw_eventtypes[] = {
@@ -128,57 +95,60 @@ static const int hw_eventtypes[] = {
 };
 
 static const int hw_absevents[] = {
-	ABS_X, ABS_Y, ABS_PRESSURE, ABS_MISC
+	ABS_X, ABS_Y, ABS_TILT_X, ABS_TILT_Y, ABS_WHEEL,
+	ABS_RX, ABS_RY, ABS_PRESSURE, ABS_MISC,
 };
 
-
-
+static const int hw_btnevents[] = {
+	BTN_STYLUS, BTN_STYLUS2, BTN_TOOL_PEN, BTN_TOOL_RUBBER,
+	BTN_TOOL_MOUSE, BTN_TOOL_FINGER,
+	BTN_0, BTN_1, BTN_2, BTN_3, BTN_4, BTN_5, BTN_6, BTN_7, BTN_8,
+};
 
 static const int hw_mscevents[] = {
 	MSC_SERIAL,
 };
 
-static void bosto_2g_parse_packet(struct bosto_2g *bosto_2g )
+static void hanwang_parse_packet(struct hanwang *hanwang)
 {
+	unsigned char *data = hanwang->data;
+	struct input_dev *input_dev = hanwang->dev;
+	struct usb_device *dev = hanwang->usbdev;
+	enum hanwang_tablet_type type = hanwang->features->type;
+	int i;
+	u16 p;
 
-	unsigned char *data = bosto_2g->data;
-	struct input_dev *input_dev = bosto_2g->dev;
-	struct usb_device *dev = bosto_2g->usbdev;
-	static u16 x = 0;
-	static u16 y = 0;
-	static u16 p = 0;
+	/* Bosto?? */
 	unsigned int pkt_type = 0x00;		// Default undefined
 	if(data[1] == 0x80) pkt_type = 1;	// Idle or tool status update in next few packets
 	if(data[1] == 0xC2) pkt_type = 2;	// tool status update packet
 	if(((data[1] & 0xF0) == 0xA0) | ((data[1] & 0xF0) == 0xE0)) pkt_type = 3;	// In proximity float 0xA0  or touch 0xE0
 
-
-	dev_dbg(&dev->dev, "Bosto_packet:  [B0:-:B8] %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x Time:%li\n",
-			data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], jiffies);
+	if (type == HANWANG_ART_MASTER_II) {
+		hanwang->current_tool = BTN_TOOL_PEN;
+		hanwang->current_id = STYLUS_DEVICE_ID;
+	}
 
 	switch (data[0]) {
-	/* pen event */
-		case 0x02:
-			switch (pkt_type) {
+	case 0x02:	/* data packet */
+		switch (pkt_type) {
+		case 1: /* tool prox out */
 
-			/* tool prox out */
-			case 1:
+			bosto_2g->stylus_btn_state = false;
+			if(bosto_2g->stylus_prox) {		// Three 0x80 indicates stylus out of proximity
+				// Release all the buttons on tool out
+				input_report_key(input_dev, BTN_STYLUS, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_STYLUS released");
+				input_report_key(input_dev, BTN_TOUCH, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOUCH released");
+				input_report_key(input_dev, BTN_TOOL_PEN, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOOL_PEN released");
+				input_report_key(input_dev, BTN_TOOL_RUBBER, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOOL_RUBBER released");
 
-				bosto_2g->stylus_btn_state = false;
-				if( bosto_2g->stylus_prox) {		// Three 0x80 indicates stylus out of proximity
-					// Release all the buttons on tool out
-					input_report_key(input_dev, BTN_STYLUS, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_STYLUS released");
-					input_report_key(input_dev, BTN_TOUCH, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOUCH released");
-					input_report_key(input_dev, BTN_TOOL_PEN, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOOL_PEN released");
-					input_report_key(input_dev, BTN_TOOL_RUBBER, 0); dev_dbg(&dev->dev, "Bosto BUTTON: BTN_TOOL_RUBBER released");
-
-					bosto_2g->current_id = 0;
-					bosto_2g->current_tool = 0;
-					bosto_2g->tool_update = 1;
-					bosto_2g->stylus_prox = false;
-					dev_dbg(&dev->dev, "Bosto TOOL OUT");
-				}
-				break;
+				bosto_2g->current_id = 0;
+				bosto_2g->current_tool = 0;
+				bosto_2g->tool_update = 1;
+				bosto_2g->stylus_prox = false;
+				dev_dbg(&dev->dev, "Bosto TOOL OUT");
+			}
+			break;
 
 			// button event
 			case 2:
@@ -269,27 +239,23 @@ static void bosto_2g_parse_packet(struct bosto_2g *bosto_2g )
 	bosto_2g->tool_update = 0;
 }
 
-static void bosto_2g_irq(struct urb *urb)
+static void hanwang_irq(struct urb *urb)
 {
-	struct bosto_2g *bosto_2g = urb->context;
-	struct usb_device *dev = bosto_2g->usbdev;
+	struct hanwang *hanwang = urb->context;
+	struct usb_device *dev = hanwang->usbdev;
 	int retval;
 
 	switch (urb->status) {
 	case 0:
 		/* success */;
-		bosto_2g_parse_packet(bosto_2g);
+		hanwang_parse_packet(hanwang);
 		break;
 	case -ECONNRESET:
 	case -ENOENT:
-	case -EINPROGRESS:
-		//dev_err(&dev->dev, "%s - urb in progress status: %d",
-		//			__func__, urb->status);
-				return;
 	case -ESHUTDOWN:
 		/* this urb is terminated, clean up */
-		//dev_err(&dev->dev, "%s - urb shutting down with status: %d",
-		//	__func__, urb->status);
+		dev_err(&dev->dev, "%s - urb shutting down with status: %d",
+			__func__, urb->status);
 		return;
 	default:
 		dev_err(&dev->dev, "%s - nonzero urb status received: %d",
@@ -303,32 +269,32 @@ static void bosto_2g_irq(struct urb *urb)
 			__func__, retval);
 }
 
-static int bosto_2g_open(struct input_dev *dev)
+static int hanwang_open(struct input_dev *dev)
 {
-	struct bosto_2g *bosto_2g = input_get_drvdata(dev);
+	struct hanwang *hanwang = input_get_drvdata(dev);
 
-	bosto_2g->irq->dev = bosto_2g->usbdev;
-	if (usb_submit_urb(bosto_2g->irq, GFP_KERNEL))
+	hanwang->irq->dev = hanwang->usbdev;
+	if (usb_submit_urb(hanwang->irq, GFP_KERNEL))
 		return -EIO;
 
-	/* printk(KERN_INFO "in bosto_2g open.\n" ); */
 	return 0;
 }
 
-static void bosto_2g_close(struct input_dev *dev)
+static void hanwang_close(struct input_dev *dev)
 {
-	struct bosto_2g *bosto_2g = input_get_drvdata(dev);
+	struct hanwang *hanwang = input_get_drvdata(dev);
 
-	usb_kill_urb(bosto_2g->irq);
+	usb_kill_urb(hanwang->irq);
 }
 
-static bool get_features(struct usb_device *dev, struct bosto_2g *bosto_2g)
+static bool get_features(struct usb_device *dev, struct hanwang *hanwang)
 {
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(features_array); i++) {
-		if (le16_to_cpu(dev->descriptor.idProduct) == features_array[i].pid) {
-			bosto_2g->features = &features_array[i];
+		if (le16_to_cpu(dev->descriptor.idProduct) ==
+				features_array[i].pid) {
+			hanwang->features = &features_array[i];
 			return true;
 		}
 	}
@@ -337,57 +303,59 @@ static bool get_features(struct usb_device *dev, struct bosto_2g *bosto_2g)
 }
 
 
-static int bosto_2g_probe(struct usb_interface *intf, const struct usb_device_id *id)
+static int hanwang_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
 	struct usb_device *dev = interface_to_usbdev(intf);
 	struct usb_endpoint_descriptor *endpoint;
-	struct bosto_2g *bosto_2g;
+	struct hanwang *hanwang;
 	struct input_dev *input_dev;
 	int error;
 	int i;
 
-	printk (KERN_INFO "Bosto_Probe checking Tablet.\n");
-	bosto_2g = kzalloc(sizeof(struct bosto_2g), GFP_KERNEL);
+	if (intf->cur_altsetting->desc.bNumEndpoints < 1)
+		return -ENODEV;
+
+	hanwang = kzalloc(sizeof(struct hanwang), GFP_KERNEL);
 	input_dev = input_allocate_device();
-	if (!bosto_2g || !input_dev) {
+	if (!hanwang || !input_dev) {
 		error = -ENOMEM;
 		goto fail1;
 	}
 
-	if (!get_features(dev, bosto_2g)) {
+	if (!get_features(dev, hanwang)) {
 		error = -ENXIO;
 		goto fail1;
 	}
 
-	bosto_2g->data = usb_alloc_coherent(dev, bosto_2g->features->pkg_len,
-					GFP_KERNEL, &bosto_2g->data_dma);
-	if (!bosto_2g->data) {
+	hanwang->data = usb_alloc_coherent(dev, hanwang->features->pkg_len,
+					GFP_KERNEL, &hanwang->data_dma);
+	if (!hanwang->data) {
 		error = -ENOMEM;
 		goto fail1;
 	}
 
-	bosto_2g->irq = usb_alloc_urb(0, GFP_KERNEL);
-	if (!bosto_2g->irq) {
+	hanwang->irq = usb_alloc_urb(0, GFP_KERNEL);
+	if (!hanwang->irq) {
 		error = -ENOMEM;
 		goto fail2;
 	}
 
-	bosto_2g->usbdev = dev;
-	bosto_2g->dev = input_dev;
+	hanwang->usbdev = dev;
+	hanwang->dev = input_dev;
 
-	usb_make_path(dev, bosto_2g->phys, sizeof(bosto_2g->phys));
-	strlcat(bosto_2g->phys, "/input0", sizeof(bosto_2g->phys));
+	usb_make_path(dev, hanwang->phys, sizeof(hanwang->phys));
+	strlcat(hanwang->phys, "/input0", sizeof(hanwang->phys));
 
-	strlcpy(bosto_2g->name, bosto_2g->features->name, sizeof(bosto_2g->name));
-	input_dev->name = bosto_2g->name;
-	input_dev->phys = bosto_2g->phys;
+	strlcpy(hanwang->name, hanwang->features->name, sizeof(hanwang->name));
+	input_dev->name = hanwang->name;
+	input_dev->phys = hanwang->phys;
 	usb_to_input_id(dev, &input_dev->id);
 	input_dev->dev.parent = &intf->dev;
 
-	input_set_drvdata(input_dev, bosto_2g);
+	input_set_drvdata(input_dev, hanwang);
 
-	input_dev->open = bosto_2g_open;
-	input_dev->close = bosto_2g_close;
+	input_dev->open = hanwang_open;
+	input_dev->close = hanwang_close;
 
 	for (i = 0; i < ARRAY_SIZE(hw_eventtypes); ++i)
 		__set_bit(hw_eventtypes[i], input_dev->evbit);
@@ -402,76 +370,67 @@ static int bosto_2g_probe(struct usb_interface *intf, const struct usb_device_id
 		__set_bit(hw_mscevents[i], input_dev->mscbit);
 
 	input_set_abs_params(input_dev, ABS_X,
-			     0, bosto_2g->features->max_x, 0, 0);
+			     0, hanwang->features->max_x, 4, 0);
 	input_set_abs_params(input_dev, ABS_Y,
-			     0, bosto_2g->features->max_y, 0, 0);
+			     0, hanwang->features->max_y, 4, 0);
+	input_set_abs_params(input_dev, ABS_TILT_X,
+			     0, hanwang->features->max_tilt_x, 0, 0);
+	input_set_abs_params(input_dev, ABS_TILT_Y,
+			     0, hanwang->features->max_tilt_y, 0, 0);
 	input_set_abs_params(input_dev, ABS_PRESSURE,
-			     0, bosto_2g->features->max_pressure, 0, 0);
+			     0, hanwang->features->max_pressure, 0, 0);
 
 	endpoint = &intf->cur_altsetting->endpoint[0].desc;
-	usb_fill_int_urb(bosto_2g->irq, dev,
+	usb_fill_int_urb(hanwang->irq, dev,
 			usb_rcvintpipe(dev, endpoint->bEndpointAddress),
-			bosto_2g->data, bosto_2g->features->pkg_len,
-			bosto_2g_irq, bosto_2g, endpoint->bInterval);
-	bosto_2g->irq->transfer_dma = bosto_2g->data_dma;
-	bosto_2g->irq->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
+			hanwang->data, hanwang->features->pkg_len,
+			hanwang_irq, hanwang, endpoint->bInterval);
+	hanwang->irq->transfer_dma = hanwang->data_dma;
+	hanwang->irq->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
 
-	error = input_register_device(bosto_2g->dev);
+	error = input_register_device(hanwang->dev);
 	if (error)
 		goto fail3;
 
-	usb_set_intfdata(intf, bosto_2g);
+	usb_set_intfdata(intf, hanwang);
 
 	return 0;
 
- fail3:	usb_free_urb(bosto_2g->irq);
- fail2:	usb_free_coherent(dev, bosto_2g->features->pkg_len,
-			bosto_2g->data, bosto_2g->data_dma);
+ fail3:	usb_free_urb(hanwang->irq);
+ fail2:	usb_free_coherent(dev, hanwang->features->pkg_len,
+			hanwang->data, hanwang->data_dma);
  fail1:	input_free_device(input_dev);
-	kfree(bosto_2g);
+	kfree(hanwang);
 	return error;
 
 }
 
-static void bosto_2g_disconnect(struct usb_interface *intf)
+static void hanwang_disconnect(struct usb_interface *intf)
 {
-	struct bosto_2g *bosto_2g = usb_get_intfdata(intf);
+	struct hanwang *hanwang = usb_get_intfdata(intf);
 
-	input_unregister_device(bosto_2g->dev);
-	usb_free_urb(bosto_2g->irq);
+	input_unregister_device(hanwang->dev);
+	usb_free_urb(hanwang->irq);
 	usb_free_coherent(interface_to_usbdev(intf),
-			bosto_2g->features->pkg_len, bosto_2g->data,
-			bosto_2g->data_dma);
-	kfree(bosto_2g);
+			hanwang->features->pkg_len, hanwang->data,
+			hanwang->data_dma);
+	kfree(hanwang);
 	usb_set_intfdata(intf, NULL);
 }
 
-static const struct usb_device_id bosto_2g_ids[] = {
-	{ BOSTO_TABLET_DEVICE(USB_VENDOR_ID_HANWANG, USB_PRODUCT_BOSTO22HD, BOSTO_TABLET_INT_CLASS, BOSTO_TABLET_INT_SUB_CLASS, BOSTO_TABLET_INT_PROTOCOL) },
+static const struct usb_device_id hanwang_ids[] = {
+	{ HANWANG_TABLET_DEVICE(USB_VENDOR_ID_HANWANG, HANWANG_TABLET_INT_CLASS,
+		HANWANG_TABLET_INT_SUB_CLASS, HANWANG_TABLET_INT_PROTOCOL) },
 	{}
 };
 
+MODULE_DEVICE_TABLE(usb, hanwang_ids);
 
-
-MODULE_DEVICE_TABLE(usb, bosto_2g_ids);
-
-static struct usb_driver bosto_2g_driver = {
-	.name		= "bosto_2g",
-	.probe		= bosto_2g_probe,
-	.disconnect	= bosto_2g_disconnect,
-	.id_table	= bosto_2g_ids,
+static struct usb_driver hanwang_driver = {
+	.name		= "hanwang",
+	.probe		= hanwang_probe,
+	.disconnect	= hanwang_disconnect,
+	.id_table	= hanwang_ids,
 };
 
-static int __init bosto_2g_init(void)
-{
-	printk(KERN_INFO "Bosto 2nd Generation USB Driver module being initialised.\n" );
-	return usb_register(&bosto_2g_driver);
-}
-
-static void __exit bosto_2g_exit(void)
-{
-	usb_deregister(&bosto_2g_driver);
-}
-
-module_init(bosto_2g_init);
-module_exit(bosto_2g_exit);
+module_usb_driver(hanwang_driver);
