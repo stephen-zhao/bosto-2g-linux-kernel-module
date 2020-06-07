54b89f526459553cac6b0b5c272d04235fe83920
Mark Ferrell <major@homeonderanged.org>
2020-06-06 18:22:59 -0700

    fix: path to our modified hanwang driver

--- load_hanwang.sh
+++ load_hanwang.sh
@@ -7,4 +7,4 @@ logger removed hanwang driver module from kernel
 /usr/local/bin/bosto-usb-detach
 logger detached usbhid driver module from hanwang device
 
-insmod /lib/modules/$(uname -r)/hanwang.ko
+insmod /lib/modules/$(uname -r)/extra/hanwang.ko
